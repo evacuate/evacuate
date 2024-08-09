64f20d44f1738c285b60ba2282479544a39d3cbd
minagishl <mvxd568.co@icloud.com>
2024-08-10 04:30:05 +0900

    Fixed so that the same region is not included.

--- src/index.ts
+++ src/index.ts
@@ -83,21 +83,21 @@ interface Point {
 }
 
 function parsePoints(points: Point[]): string {
-  const scaleMap: { [key: string]: string[] } = {};
+  const scaleMap: { [key: string]: Set<string> } = {};
   for (const point of points) {
     const pref = point.pref;
     const scale = parseScale(point.scale);
     if (scale !== undefined) {
       if (!scaleMap[scale]) {
-        scaleMap[scale] = [];
+        scaleMap[scale] = new Set();
       }
-      scaleMap[scale].push(translate(pref));
+      scaleMap[scale].add(translate(pref));
     }
   }
 
   const pointsInfo: string[] = [];
   for (const scale in scaleMap) {
-    const regions = scaleMap[scale].join(', ');
+    const regions = Array.from(scaleMap[scale]).join(', ');
     pointsInfo.push(`[Seismic Intensity ${scale}] ${regions}`);
   }
 
