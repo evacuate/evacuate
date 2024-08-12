58af72890107e29a502abb72f43f5bfa40df7ded
みなぎし <mvxd568.co@icloud.com>
2024-08-12 09:23:02 +0000

    Fixing the code to make it more manageable

--- src/helpers/messageCreator.ts
+++ src/helpers/messageCreator.ts
@@ -1,3 +1,20 @@
+function createBaseMessage(
+  time: string,
+  info: string | undefined,
+  body: string[],
+  isDev: boolean,
+): string {
+  const message = [`${time} ${info}`];
+
+  if (isDev) {
+    message.push('### This information is a test distribution ###');
+  }
+
+  message.push(...body, '#evacuate');
+
+  return message.join('\n');
+}
+
 export function createMessage(
   time: string,
   info: string | undefined,
@@ -5,24 +22,9 @@ export function createMessage(
   points: string,
   isDev: boolean,
 ): string {
-  if (isDev) {
-    return [
-      `${time} ${info}`,
-      '### This information is a test distribution ###',
-      `Maximum Seismic Intensity ${scale}`,
-      '',
-      `${points}`,
-      '#evacuate',
-    ].join('\n');
-  } else {
-    return [
-      `${time} ${info}`,
-      `Maximum Seismic Intensity ${scale}`,
-      '',
-      `${points}`,
-      '#evacuate',
-    ].join('\n');
-  }
+  const body = [`Maximum Seismic Intensity ${scale}`, '', `${points}`];
+
+  return createBaseMessage(time, info, body, isDev);
 }
 
 export function createTsunamiMessage(
@@ -31,22 +33,11 @@ export function createTsunamiMessage(
   area: string,
   isDev: boolean,
 ): string {
-  if (isDev) {
-    return [
-      `${time} ${info}`,
-      '### This information is a test distribution ###',
-      'There is new information in the following areas',
-      '',
-      `${area}`,
-      '#evacuate',
-    ].join('\n');
-  } else {
-    return [
-      `${time} ${info}`,
-      'There is new information in the following areas',
-      '',
-      `${area}`,
-      '#evacuate',
-    ].join('\n');
-  }
+  const body = [
+    'There is new information in the following areas',
+    '',
+    `${area}`,
+  ];
+
+  return createBaseMessage(time, info, body, isDev);
 }
