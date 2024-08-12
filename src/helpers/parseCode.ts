4c8e2f338e72e90dd6f8ba91ce3ee52261b10e42
みなぎし <mvxd568.co@icloud.com>
2024-08-12 06:03:00 +0000

    Code Optimization

--- src/helpers/parseCode.ts
+++ src/helpers/parseCode.ts
@@ -1,18 +1,12 @@
+const codeMap = new Map<number, string>([
+  [551, 'Earthquake Information'],
+  [552, 'Tsunami Information'],
+  [554, 'Earthquake Early Warning Detected'],
+  [556, 'Earthquake Early Warning (Alert)'],
+  [561, 'Earthquake Detection Information'],
+  [9611, 'Earthquake Detection Analysis Results'],
+]);
+
 export default function parseCode(code: number): string | undefined {
-  switch (code) {
-    case 551:
-      return 'Earthquake Information';
-    case 552:
-      return 'Tsunami Information';
-    case 554:
-      return 'Earthquake Early Warning Detected';
-    case 556:
-      return 'Earthquake Early Warning (Alert)';
-    case 561:
-      return 'Earthquake Detection Information';
-    case 9611:
-      return 'Earthquake Detection Analysis Results';
-    default:
-      return undefined;
-  }
+  return codeMap.get(code);
 }
