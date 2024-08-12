9553398c44dd5fb07d26f0e780bcfde6ea9813c2
みなぎし <mvxd568.co@icloud.com>
2024-08-12 09:18:32 +0000

    Accelerate code execution speed

--- src/helpers/parseScale.ts
+++ src/helpers/parseScale.ts
@@ -1,24 +1,15 @@
+const scaleMap = new Map<number, string>([
+  [10, '1'],
+  [20, '2'],
+  [30, '3'],
+  [40, '4'],
+  [45, '5 weak'],
+  [50, '5 strong'],
+  [55, '6 weak'],
+  [60, '6 strong'],
+  [70, '7'],
+]);
+
 export default function parseScale(scale: number): string | undefined {
-  switch (scale) {
-    case 10:
-      return '1';
-    case 20:
-      return '2';
-    case 30:
-      return '3';
-    case 40:
-      return '4';
-    case 45:
-      return '5 weak';
-    case 50:
-      return '5 strong';
-    case 55:
-      return '6 weak';
-    case 60:
-      return '6 strong';
-    case 70:
-      return '7';
-    default:
-      return undefined;
-  }
+  return scaleMap.get(scale);
 }
